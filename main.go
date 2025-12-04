4064c8fab4aca9648aa79cd5d68abe5c9a9bb63a
David Zarzycki <david.zarzycki@gmail.com>
2025-12-04 15:45:06 -0500

    Update main.go

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ func main() {
 	// Initialize the plugin set and register the builder, provisioner, post-processor, and datasource.
 	pps := plugin.NewSet()
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(vergeio.Builder))
-	// pps.RegisterBuilder("farooq-builder", new(vergeio.Builder))
 	pps.RegisterProvisioner("my-provisioner", new(vergeioProv.Provisioner))
 	pps.RegisterPostProcessor("my-post-processor", new(vergeioPP.PostProcessor))
 	pps.RegisterDatasource("my-datasource", new(vergeioData.Datasource))
